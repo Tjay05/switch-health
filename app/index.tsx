4fcc24fde4994aa79ff7e517165d8a9967f593bd
Olatayo Tijesu <tjayolatayo@gmail.com>
2024-06-18 01:13:14 +0100

    First commit

---
+++ app/index.tsx
@@ -0,0 +1,18 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: 'red'
+      }}
+    >
+      <Text style={{color: 'pink', width: '70%', marginLeft: 'auto', marginRight: 'auto'}}>Hello world😁 Lorem ipsumm tenetur quaerat, dolorum odit nemo deleniti cum, tempore officia, voluptas illum ullam natus perferenduas ipsum veniam, at incidunt qui omnis nam voluptate iste totam?
+      </Text>
+      <Text>balablooo </Text>
+    </View>
+  );
+}

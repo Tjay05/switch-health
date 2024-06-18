fd904cf10d7d0c162f67f4526534c53c21a40b9b
Tjay05 <tjayolatayo@gmail.com>
2024-06-18 02:49:31 +0100

    Add basic packages

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,34 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import { styled } from "styled-components";
 
 export default function Index() {
+  const styles = StyleSheet.create({
+    box: {
+      // flex: 1, 
+      color: '#ffffff',
+      textAlign: 'left',
+      textDecorationLine: 'underline line-through'  
+    }
+  })
+
+  const Overview = styled.View`
+    flex: 1;
+    justify-content: center;
+    background-color: red;
+    align-items: center;
+  `;
+
+  const Paragraph = styled.Text`
+    color: blue;
+    width: 60%;
+    margin-inline: auto;
+  `;
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: 'red'
-      }}
-    >
-      <Text style={{color: 'pink', width: '70%', marginLeft: 'auto', marginRight: 'auto'}}>Hello world😁 Lorem ipsumm tenetur quaerat, dolorum odit nemo deleniti cum, tempore officia, voluptas illum ullam natus perferenduas ipsum veniam, at incidunt qui omnis nam voluptate iste totam?
-      </Text>
-      <Text>balablooo </Text>
-    </View>
+    <Overview>
+      <Paragraph>Hello world😁 Lorem ipsumm tenetur quaerat, dolorum odit nemo deleniti cum, tempore officia, voluptas illum ullam natus perferenduas ipsum veniam, at incidunt qui omnis nam voluptate iste totam?
+      </Paragraph>
+      <Text style={styles.box}>balablooo </Text>
+    </Overview>
   );
 }

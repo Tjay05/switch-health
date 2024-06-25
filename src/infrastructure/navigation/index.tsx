fb5fece4a8bf1c8233947eb472d3f5a42ebb4a74
Tjay05 <tjayolatayo@gmail.com>
2024-06-25 01:38:24 +0100

    Add home page

---
+++ src/infrastructure/navigation/index.tsx
@@ -0,0 +1,13 @@
+import AccountNavigator from "./Account.navigator";
+import AppNavigator from "./AppNavigator";
+
+const Navigation = () => {
+  return (
+    <>
+      {/* <AccountNavigator/> */}
+      <AppNavigator/>
+    </>
+  );
+}
+ 
+export default Navigation;

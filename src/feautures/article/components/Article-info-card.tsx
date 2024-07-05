ce2dab046029da487f8247f3dde6c6a8922f969f
tosxin-456 <tosinekshally@gmail.com>
2024-07-05 21:01:26 +0100

    feat: consumed the artice api

--- src/feautures/article/components/Article-info-card.tsx
+++ src/feautures/article/components/Article-info-card.tsx
@@ -1,43 +1,54 @@
 import Text from "@/src/components/typograpghy/Text.component";
-import { 
+import {
   ArtFooter,
   ArticleCard,
   ArticleImg,
   ArticleTextWrap,
   ArticleWrapper,
   CardContainer,
-  Date,
-  Title
+  Date as ArticleDate,
+  Title,
 } from "./Article.styles";
 
 import { Ionicons } from "@expo/vector-icons";
 import DocImg from "../../../../assets/images/doctor.png";
 
-const ArticleInfo = ({ article ={} }) => {
+const ArticleInfo = ({ article = {} }) => {
   const {
-    image = DocImg,
+    image = "https://res.cloudinary.com/dba1aezsn/image/upload/v1720109527/24_Secrets_Pain_Doctors_Won_t_Tell_You_aiwlxf.jpg",
     title = "Dental Health a Part to Success",
-    date = "Ju1 10, 2023",
+    createdAt = "2024-07-04T16:34:44.298+00:00",
     readTime = "5min read",
   } = article;
 
-  return (  
+  const formatDate = (dateString) => {
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  };
+
+  return (
     <ArticleWrapper>
       <ArticleCard>
         <CardContainer>
-          <ArticleImg source={image}/>
+          <ArticleImg source={{ uri: image }} />
           <ArticleTextWrap>
-            <Title>{title}</Title>
+            <Title>
+              <Text>{title}</Text>
+            </Title>
             <ArtFooter>
-              <Date>{date}</Date>
-              <Date>{readTime}</Date>
+              <ArticleDate>
+                <Text>{formatDate(createdAt)}</Text>
+              </ArticleDate>
+              <ArticleDate>
+                <Text>{readTime}</Text>
+              </ArticleDate>
             </ArtFooter>
           </ArticleTextWrap>
         </CardContainer>
-        <Ionicons name="bookmark-outline" size={20} color='#407CE2' />
+        <Ionicons name="bookmark-outline" size={20} color="#407CE2" />
       </ArticleCard>
     </ArticleWrapper>
   );
-}
- 
+};
+
 export default ArticleInfo;

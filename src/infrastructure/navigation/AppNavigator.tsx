7b1ce2d2ece75e8aaa4e28509c38c9d8620693d6
Tjay05 <tjayolatayo@gmail.com>
2024-07-08 11:41:58 +0100

    Clean up tab navigator

--- src/infrastructure/navigation/AppNavigator.tsx
+++ src/infrastructure/navigation/AppNavigator.tsx
@@ -7,71 +7,69 @@ import HomeNavigator from "./Home.navigator";
 import ProfileNavigator from "./Profile.navigator";
 import ReportNavigator from "./Report.navigator";
 
+const Tab = createBottomTabNavigator();
+
 const AppNavigator = () => {
-  const Tab = createBottomTabNavigator();
-
   return (
-    <>
-      <Tab.Navigator 
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
+    <Tab.Navigator 
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
 
-            if (route.name === 'Home') {
-              if (iconName = focused) {
-                return <Ionicons name="home" size={size} color={color}/>
-              } else {
-                return <Ionicons name="home" size={size} color={color}/>
-              }
-            } else if (route.name === 'Reports') {
-              if (iconName = focused) {
-                return <MaterialCommunityIcons name="clipboard-pulse" size={size} color={color}/>
-              } else {
-                return <MaterialCommunityIcons name="clipboard-pulse-outline" size={size} color={color}/>
-              }
-            } else if (route.name === 'Appointments') {
-              if (iconName = focused) {
-                return <MaterialCommunityIcons name="clipboard-text" size={size} color={color}/>
-              } else {
-                return <MaterialCommunityIcons name="clipboard-text-outline" size={size} color={color}/>
-              }
-            } else if (route.name === 'Profile') {
-              if (iconName = focused) {
-                return <MaterialCommunityIcons name="account" size={size} color={color}/>
-              } else {
-                return <MaterialCommunityIcons name="account-outline" size={size} color={color}/>
-              }
+          if (route.name === 'Home') {
+            if (iconName = focused) {
+              return <Ionicons name="home" size={size} color={color}/>
+            } else {
+              return <Ionicons name="home-outline" size={size} color={color}/>
             }
-          },
-          tabBarActiveTintColor: '#1A1F71',
-          tabBarInactiveTintColor: '#221F1F99',
-          headerShown: false,
-          tabBarLabelStyle: {
-            fontFamily: theme.fonts.poppinsMedium,
-          },
-          tabBarStyle: {
-            paddingBottom: 2,
-          },
-        })}
-      >
-        <Tab.Screen 
-          name="Home" 
-          component={HomeNavigator} 
-        />
-        <Tab.Screen 
-          name="Reports" 
-          component={ReportNavigator} 
-        />
-        <Tab.Screen 
-          name="Appointments"
-          component={AppointmentNavigator} 
-        />
-        <Tab.Screen 
-          name="Profile" 
-          component={ProfileNavigator} 
-        />
-      </Tab.Navigator>
-    </>
+          } else if (route.name === 'Reports') {
+            if (iconName = focused) {
+              return <MaterialCommunityIcons name="clipboard-pulse" size={size} color={color}/>
+            } else {
+              return <MaterialCommunityIcons name="clipboard-pulse-outline" size={size} color={color}/>
+            }
+          } else if (route.name === 'Appointments') {
+            if (iconName = focused) {
+              return <MaterialCommunityIcons name="clipboard-text" size={size} color={color}/>
+            } else {
+              return <MaterialCommunityIcons name="clipboard-text-outline" size={size} color={color}/>
+            }
+          } else if (route.name === 'Profile') {
+            if (iconName = focused) {
+              return <MaterialCommunityIcons name="account" size={size} color={color}/>
+            } else {
+              return <MaterialCommunityIcons name="account-outline" size={size} color={color}/>
+            }
+          }
+        },
+        tabBarActiveTintColor: '#1A1F71',
+        tabBarInactiveTintColor: '#221F1F99',
+        headerShown: false,
+        tabBarLabelStyle: {
+          fontFamily: theme.fonts.poppinsMedium,
+        },
+        tabBarStyle: {
+          paddingBottom: 2,
+        },
+      })}
+    >
+      <Tab.Screen 
+        name="Home" 
+        component={HomeNavigator} 
+      />
+      <Tab.Screen 
+        name="Reports" 
+        component={ReportNavigator} 
+      />
+      <Tab.Screen 
+        name="Appointments"
+        component={AppointmentNavigator} 
+      />
+      <Tab.Screen 
+        name="Profile" 
+        component={ProfileNavigator} 
+      />
+    </Tab.Navigator>
   );
 }
  

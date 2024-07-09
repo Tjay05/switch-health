b20d014b7c581dcb5c2ab99b07f931caecafd183
Tjay05 <tjayolatayo@gmail.com>
2024-07-09 13:38:55 +0100

    Add notifications proper page

--- src/feautures/home/components/Notification.styles.tsx
+++ src/feautures/home/components/Notification.styles.tsx
@@ -2,7 +2,7 @@ import Text from '@/src/components/typograpghy/Text.component';
 import { SafeAreaView, ScrollView, View } from 'react-native';
 import styled from 'styled-components';
 
-export const NotContainer = styled(SafeAreaView)`
+export const NotContainer = styled(ScrollView)`
   flex: 1;
   background-color: #fff;
   width: 100%;
@@ -14,6 +14,54 @@ export const NotWrapper = styled(View)`
   margin-horizontal: auto;
 `;
 
+export const NotificationSection = styled(View)`
+  width: 100%;
+`;
+
+export const DateLabel = styled(Text)`
+  background-color: #407CE20D;
+  padding: 6px 9px;
+  border-radius: 11px;
+  align-self: center;
+  font-family: ${(props) => props.theme.fonts.poppinsMedium};
+`;
+
+export const NotificationHeader = styled(View)`
+  width: 100%;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const NotText = styled(Text)`
+  font-size: ${(props) => props.theme.fontSizes.info};
+`;
+
+export const TimeLabel = styled(Text)`
+  font-size: 14px;
+  color: #000000B5;
+  font-family: ${(props) => props.theme.fonts.poppinsRegular};
+`;
+
+export const NotificationBox= styled(View)`
+  flex-direction: row;
+  width: 100%;
+  align-items: center;
+  gap: ${(props) => props.theme.space[3]};
+  background-color: #fff;
+  border-bottom-color: #00000012;
+  border-bottom-width: 1px;
+  padding: ${(props) => props.theme.space[2]}
+`;
+
+export const NotificationText = styled(Text)`
+  color: #565656;
+  font-size: ${(props) => props.theme.fontSizes.body};
+  flex: 1;
+  font-family: ${(props) => props.theme.fonts.poppinsRegular};
+`;
+
+// Notification Settings styles
 export const Section = styled(View)`
   align-items: center;
   justify-content: space-between;
--- src/feautures/home/screens/Notification.screen.tsx
+++ src/feautures/home/screens/Notification.screen.tsx
@@ -1,24 +1,84 @@
 import NoNotSVG from "@/assets/icons/NoNotification";
 import Spacer from "@/src/components/spacer/Spacer.component";
 import Text from "@/src/components/typograpghy/Text.component";
+import { useState } from "react";
 import { 
   NoAppWrapper
 } from "../../appointment/components/Appointment.styles";
-import { NotContainer, NotWrapper } from "../components/Notification.styles";
+import { 
+  DateLabel, 
+  NotContainer, 
+  NotificationBox, 
+  NotificationHeader, 
+  NotificationSection, 
+  NotificationText, 
+  NotText, 
+  NotWrapper, 
+  TimeLabel 
+} from "../components/Notification.styles";
+import { Ionicons } from "@expo/vector-icons";
 
 const NotificationScreen = () => {
+  const [notifications, setNotifications] = useState([]);
+
   return (
     <>
-      <NotContainer>
+      <NotContainer showsVerticalScrollIndicator={false}>
         <NotWrapper>
-          <NoAppWrapper>
-            <Spacer position="bottom" size="extraLarge">
-              <NoNotSVG/>
-            </Spacer>
-            <Spacer position="bottom" size="large">
-              <Text variant='place'>All your notifications will appear here</Text>
-            </Spacer>
-          </NoAppWrapper>
+          {!notifications.length ? (
+            <NotificationSection>
+              <Spacer position="top" size="large">
+                <DateLabel>June 2024</DateLabel>
+              </Spacer>
+              <Spacer position="top" size="large">
+                <NotificationHeader>
+                  <NotText variant='label'>Reminder</NotText>
+                  <TimeLabel>Today at 5:12am</TimeLabel>
+                </NotificationHeader>
+                <Spacer position="top" size="large">
+                  <NotificationBox>
+                    <Ionicons name="notifications" size={28} color="#263A5C" />
+                    <NotificationText>Your appointment with Dr. Angel is tomorrow at 2:00 PM</NotificationText>
+                  </NotificationBox>
+                </Spacer>
+              </Spacer>
+              <Spacer position="top" size="large">
+                <NotificationHeader>
+                  <TimeLabel>Today at 4:00am</TimeLabel>
+                </NotificationHeader>
+                <Spacer position="top" size="large">
+                  <NotificationBox>
+                    <Ionicons name="notifications" size={28} color="#263A5C" />
+                    <NotificationText>Your appointment with Dr. Angel is tomorrow at 2:00 PM</NotificationText>
+                  </NotificationBox>
+                </Spacer>
+              </Spacer>
+              <Spacer position="top" size="extraLarge">
+                <DateLabel>May 2024</DateLabel>
+              </Spacer>
+              <Spacer position="top" size="large">
+                <NotificationHeader>
+                  <NotText variant='label'>Welcome</NotText>
+                  <TimeLabel>11/05/24</TimeLabel>
+                </NotificationHeader>
+                <NotificationBox>
+                  <Ionicons name="notifications" size={28} color="#263A5C" />
+                  <NotificationText>Welcome to switch Health, Tosin Poppins we are thrilled to have you.</NotificationText>
+                </NotificationBox>
+              </Spacer>
+            </NotificationSection>
+          ) : (
+            <NoAppWrapper>
+              <Spacer position="top" size="XXL">
+                <Spacer position="bottom" size="extraLarge">
+                  <NoNotSVG/>
+                </Spacer>
+                <Spacer position="bottom" size="large">
+                  <Text variant='place'>All your notifications will appear here</Text>
+                </Spacer>
+              </Spacer>
+            </NoAppWrapper>
+          )}
         </NotWrapper>
       </NotContainer>
     </>

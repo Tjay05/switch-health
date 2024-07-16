a5d0ed82970c369555cbd168e415d90e6cb48eff
Tjay05 <tjayolatayo@gmail.com>
2024-07-16 21:55:55 +0100

    Add payment feature to cart

--- src/feautures/pharmacy/screens/Cart.tsx
+++ src/feautures/pharmacy/screens/Cart.tsx
@@ -1,17 +1,72 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { FontAwesome6, Ionicons, SimpleLineIcons } from "@expo/vector-icons";
+import { Alert, Button, Modal, StyleSheet, TextInput, TouchableOpacity, View } from "react-native";
+import { Paystack, paystackProps } from "react-native-paystack-webview";
+
 import Spacer from "@/src/components/spacer/Spacer.component";
 import Text from "@/src/components/typograpghy/Text.component";
 import { LogBtn } from "../../account/components/account.styles";
 import { NoAppWrapper } from "../../appointment/components/Appointment.styles";
 import { CartDescription, CartDetailWrap, CartFooter, CartImg, CartItemBox, CartItemDetails, CartItemPrice, CartItems, CartPaymentWrapper, CartQtyWrap, CartWrapper, ItemName, ItemSize, PayItems, PayTextHead, PayTexts, PharmacyContainer, PharmacyWrapper, PlusWrap, QtySelector, SummaryWrap, TotalText } from "../components/Pharmacy.styles";
 
-import { FontAwesome6, Ionicons, SimpleLineIcons } from "@expo/vector-icons";
-import { TouchableOpacity } from "react-native";
 import EmptyCartSVG from "@/assets/icons/EmptyCartSvg";
+import { theme } from "@/src/infrastructure/theme";
 
 const CartScreen = ({ navigation }) => {
+  const paystackWebViewRef = useRef<paystackProps.PayStackRef>();
+
   const [cart, setCart] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [userData, setUserData] = useState(null);
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState("");
+
+  const getData = async () => {
+    try {
+      const storedData = await AsyncStorage.getItem("data");
+      if (storedData !== null) {
+        setUserData(JSON.parse(storedData));
+      }
+    } catch (error) {
+      console.error("Error retrieving stored data:", error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  useEffect(() => {
+    if (userData) {
+      handleGetDetails();
+    }
+  }, [userData]);
+
+  const handleGetDetails = async () => {
+    try {
+      const response = await fetch(
+        `https://switch-health.onrender.com/patient/${userData.data.user._id}/profile`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${userData.data.accessToken}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        setEmail(data.data.email || "");
+        setNumber(data.data.phone || "");
+      } else {
+        console.error("Failed to fetch profile data:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error fetching profile data:", error);
+    }
+  };
 
   useEffect(() => {
     const fetchCart = async () => {
@@ -110,8 +165,53 @@ const CartScreen = ({ navigation }) => {
                 <PayTexts>Total</PayTexts>
                 <TotalText>₦{totalAmount}</TotalText>
               </SummaryWrap>
-              <LogBtn>Checkout</LogBtn>
+              <Paystack
+                paystackKey="pk_live_12c52b7e3887c200b19a101a9273313807053282"
+                billingEmail={email}
+                amount={totalAmount}
+                activityIndicatorColor='red'
+                onCancel={(e) => {
+                  console.log(e);
+                }}
+                onSuccess={(res) => {
+                  Alert.alert(`succesful: ${res}`)
+                }}
+                ref={paystackWebViewRef}
+              />
+              <LogBtn onPress={() => setModalVisible(true)}>Checkout</LogBtn>
             </CartFooter>
+
+            <Modal
+              transparent={true}
+              visible={modalVisible}
+              onRequestClose={() => setModalVisible(false)}
+            >
+              <View style={styls.modalBackground}>
+                <View style={styls.modalContainer}>
+                  <Text variant='place'>Confirm details to make payment</Text>
+                  <Text style={styls.labbel}>Email</Text>
+                  <TextInput
+                    value={email}
+                    style={styls.inputfield}
+                  />
+                  {/* <Text style={styls.labbel}>Phone number</Text>
+                  <TextInput
+                    value={number}
+                    style={styls.inputfield}
+                  /> */}
+                  <Text style={styls.labbel}>Amount</Text>
+                  <TextInput
+                    value={`₦${totalAmount}`}
+                    style={styls.inputfield}
+                  />
+                  <View style={styls.buttonContainer}>
+                    <Button title="Cancel" onPress={() => setModalVisible(false)} />
+                    <Button title="Confirm Payment" onPress={() => paystackWebViewRef.current?.startTransaction()} />
+                  </View>
+                </View>
+              </View>
+            </Modal>
+
           </CartWrapper>
         ) : (
           <NoAppWrapper>
@@ -128,5 +228,42 @@ const CartScreen = ({ navigation }) => {
     </PharmacyWrapper>
   );
 }
+
+const styls = StyleSheet.create({
+  modalBackground: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  }, 
+  modalContainer: {
+    width: 300,
+    padding: 20,
+    backgroundColor: 'white',
+    borderRadius: 10,
+  }, 
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 20,
+  },
+  inputfield: {
+    width: '100%',
+    height: 35,
+    marginBottom: 4,
+    backgroundColor: '#F9FAFB',
+    borderRadius: 20,
+    paddingHorizontal: 12,
+    borderColor: 'hsla(0, 0%, 62%, 1)',
+    color: 'hsla(0, 0%, 62%, 1)',
+    borderWidth: 1,
+    fontFamily: theme.fonts.poppinsRegular,
+  },
+  labbel: {
+    fontSize: 14,
+    color: 'hsla(0, 0%, 42%, 1)',
+    fontFamily: theme.fonts.poppinsMedium,
+  }
+});
  
 export default CartScreen;
